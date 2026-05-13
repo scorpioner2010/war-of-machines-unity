b31c174d2ffb3ff2675c81870f88befdaf324d35
Taras Samaryn <samarintaras@gmail.com>
2026-05-14 01:01:22 +0800

    added build profile

--- Assets/Game/Scripts/API/HttpLink.cs
+++ Assets/Game/Scripts/API/HttpLink.cs
@@ -2,6 +2,8 @@ namespace Game.Scripts.API
 {
     public abstract class HttpLink
     {
-        public static string APIBase = "https://localhost:44377";
+        public const string LocalAPIBase = "https://localhost:44377";
+        public const string RenderAPIBase = "https://war-of-machines-api.onrender.com";
+        public static string APIBase = RenderAPIBase;
     }
 }
